074099af5621c5eff30b2097e767b7d5cecc2317
emmabailey-uni <emma.bailey@ucdconnect.ie>
2019-08-27 15:44:45 +0100

    Updates in launch files, commented code

--- src/path_following_navigation.cpp
+++ src/path_following_navigation.cpp
@@ -35,23 +35,29 @@ class PathFollowingController
 private:
     ros::NodeHandle n;
 
+    //Publisher topics
     ros::Publisher cmd_vel_pub;
+
+    //Subscriber topics
     ros::Subscriber map_sub;
     ros::Subscriber odom_sub;
 
+    //Variables to discretize map into pixels
     int Width;
     int Height;
     float cell_size;
 
+    //End position of robot - set from launch
     int endX, endY;
+
+    //Start position of robot - set from launch
     int startX, startY;
 
     float x, y, theta;
 
-
     bool robot_stopped;
 
-    // Desired distance to the right wall
+    // Desired distance to the right wall - set from launch
     float desired_linear_velocity;
     float desired_side_wall_distance;
     float front_obstacle_distance_threshold;
@@ -67,13 +73,21 @@ private:
 
     float x_offset, y_offset;
 
-
+    //enum is a data type in c++ that contains a set of statis constants - not reinitialised on each run (they remember)
+    //Essentially used here as a fancy switch_flag
     enum CONTROLLER_STATE
-    {   ROTATE = 1,
+    {
+        //Rotate means robot will be turning, follow line means it goes straight
+        ROTATE = 1,
         FOLLOW_LINE = 2,
     };
 
+    //Combines two values in a list - inextricably tied to each other in each list position, variable name is final_path
+    //Final optimal path
      std::list<std::pair<int,int>> final_path;
+
+     //Path we're currently running on to determine optimal
+     std::pair<int,int> my_p;
 
 
     bool init_flag = false;
@@ -81,9 +95,7 @@ private:
     bool path_done = false;
     bool pos_set = false;
 
-    std::pair<int,int> my_p;
 
-    
     geometry_msgs::Twist calculateCommand()
     {
         auto square_vel_msg = geometry_msgs::Twist();
@@ -95,12 +107,11 @@ private:
         static int i = 0;
         static CONTROLLER_STATE ControllerState;
 
-
-
-
         if(!init_flag){
+          //To keep track of where we are in map
           x_start = 0;
           y_start = 0;
+          //Init flag changed - won't reinitilaise to start every time
           init_flag = true;
           ControllerState = ROTATE;
           my_p = final_path.front();
@@ -108,10 +119,13 @@ private:
           //std::cout << "FIRST GOAL" << (final_path.front()).first << "\n";
         }
 
+        //Will enter here as done_flag set to true at start of this function
         if(done_flag){
+            //This conditon only entered when robot has arrived at the exit as we continually pop the queue
             if(final_path.empty()){
                 //std::cout << "List is empty\n" ;
             }else{
+                //This part determines the instructions to send to driver
                 my_p = final_path.front();
                 final_path.pop_front();
                 x_goal = Width*cell_size-(Width-(my_p.second))*cell_size+cell_size/2;
@@ -122,7 +136,6 @@ private:
 
         //std::cout<<"Theta current: "<<theta<<"\n";
 
-
         theta_ref = atan2((y_goal-y),(x_goal-x));
         //std::cout << "x_goal " << x_goal << " y_goal"  << y_goal <<"\n";
         //std::cout << "x_curr " << x << " y_curr"  << y <<"\n";
@@ -130,22 +143,26 @@ private:
 
 
         switch (ControllerState) {
-
+          //Rotate case - need to spin
           case 1 :
             //std::cout << "ROTATE" << "\n";
             //std::cout << "Diff" << (theta - theta_ref) <<"\n";
+            //Angle not reduced enough - need to continue turning
             if (theta - theta_ref > 0.05 || theta - theta_ref < -0.05 ){
                 v = K_omega * (cos(theta)*(x_start-x) + sin(theta)*(y_start-y));
                 w = K_psi*(theta_ref-theta);
             } else{
+              //Turn complete - want to stop and enter new case on next run
                 w = 0;
                 v = 0;
                 ControllerState = FOLLOW_LINE;
+                //Update position in map - need to always know what pixel we are in
                 x_start = x;
                 y_start = y;
             }
             break;
 
+          //Want to move in a straight line to next positon
           case 2 :
             //std::cout << "FOLLOW LINE" << "\n";
             if(cos(theta)*(x_goal-x) + sin(theta)*(y_goal-y) > 0.05 || cos(theta)*(x_goal-x) + sin(theta)*(y_goal-y) < -0.05 ){
@@ -154,7 +171,9 @@ private:
             } else{
                 w = 0;
                 v = 0;
+                //Need to reset done_flag in order to continue sending info
                 done_flag = true;
+                //Update positon in map
                 x_start = x;
                 y_start = y;
                 ControllerState = ROTATE;
@@ -162,47 +181,55 @@ private:
             break;
         }
 
+        //Send the desired velocities to the robot driver based on current position and motion needs
         square_vel_msg.linear.x = v;
         square_vel_msg.angular.z = w;
         return square_vel_msg;
     }
 
-
-
+    //Gets the map from the basic maze, discretizes it, all info in grids. Relates all pixels with distance value to compute cost function.
+    //Wave Propagation algorithm - check gc video
     void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg){
         std_msgs::Header header = msg->header;
         nav_msgs::MapMetaData info = msg->info;
         ROS_INFO("Got map %d %d", info.width, info.height);
 
+        //Cell size determined by our desired resolution
         cell_size = info.resolution;
+        //Know width and height of map from message - assigned to variable
         Width = info.width, Height = info.height;
         int cols = info.width, rows = info.height;
         auto p = [&](int x, int y) { return x * cols + y;  };
 
-        //Binary map
+        //Need matrices with correct numbers of rows and columns to discretise entire map - all 2D mappings - remember pointers
+        //Binary Map - keeps track of whether postion is an obstacle or not
         int** M = new int*[rows];
         for (int i = 0; i < rows; ++i){
             M[i] = new int[cols];
         }
 
-        //Potential filed
+        //Potential field - keeps track of each potential path through maze
         int** C = new int*[rows];
         for (int i = 0; i < rows; ++i){
             C[i] = new int[cols];
         }
 
-        //Shortest path
+        //Shortest path - will store the optimal path determined
         int** P = new int*[rows];
         for (int i = 0; i < rows; ++i){
             P[i] = new int[cols];
         }
-        
+
         std::cout << "\n\n BINARY MAP \n\n";
+        //Cycle through each row and column
         for (int i = (rows-1); i >= 0; --i) {   // for each row
             for (int j = 0; j < cols; ++j) { // for each column
+                //If theres something in the cell (NOT empty space) - set value to 1
                 if(msg->data[i*cols + j] > 0){
                     M[i][j] = 1;
-                } else{
+                }
+                //If its empty space set a zero
+                else{
                     M[i][j] = 0;
                 }
                 std::cout << M[i][j] << " ";
@@ -210,6 +237,7 @@ private:
             std::cout << "\n";
         }
 
+        //Changing obstacles to be -1 to make wave propagation easier - limits of map hard set to 0 too
         for(int x = (rows-1); x >= 0; --x){
             for(int y = 0; y < cols; y++){
                 if(x == 0 || y == 0 || x == (cols -1) || y == (rows -1) || M[x][y] == 1){
@@ -222,21 +250,25 @@ private:
 
 
 
-        
+        //New paired listing of three values for x, y and distance
         std::list<std::tuple<int,int,int>> nodes;
 
+        //Exit of the map pushed to the list first
         nodes.push_back({endX,endY,1});
 
         while(!nodes.empty()){
+            //New paired listing of three values for x, y and distance
             std::list<std::tuple<int,int,int>> new_nodes;
 
             for(auto &n : nodes){
+                //This is the method to access the variable held in our tuple list
                 int x = std::get<0>(n);
                 int y = std::get<1>(n);
                 int d = std::get<2>(n);
 
                 C[x][y] = d;
 
+                //Determining cost map - moving through maps possible paths and adding one to distance with each propgation move
                 // Check south
                 if((x+1) < rows && C[x+1][y] == 0){
                     new_nodes.push_back({x+1,y,d+1});
@@ -295,10 +327,10 @@ private:
             nodes.insert(nodes.begin(), new_nodes.begin(), new_nodes.end());
         }
 
-
+        /*
         std::cout << "\n\n VISUALIZABLE BINARY \n\n";
-        
 
+        //Prints out the binary map
         for(int x = 0; x < rows; x++){
             for(int y = 0; y < cols; y++){
                 //std::cout << C[x][y] << " ";
@@ -311,10 +343,13 @@ private:
             }
             std::cout << "\n";
         }
+        */
 
-
+        //Algorithm to move through the cost map - choosing optimal path
+        //Pushing new x-y coord each time you move
         std::list<std::pair<int,int>> path;
         path.push_back({startX,startY});
+        //locx anf locy keep track of current location
         int locX = startX;
         int locY = startY;
         bool no_path = false;
@@ -322,6 +357,7 @@ private:
         while(!(locX == endX && locY == endY) && !no_path){
             std::list<std::tuple<int,int,int>> listNeighbours;
 
+            //This is the bit we want to optimise - we think
             // Check south
             if((locX+1) < rows && C[locX+1][locY] > 0){
                 listNeighbours.push_back({locX+1,locY,C[locX+1][locY]});
@@ -378,12 +414,12 @@ private:
         for (auto &a : path){
             p_x = a.first;
             p_y = a.second;
-            std::cout << "X" << p_x << " Y " << p_y << "\n"; 
+            std::cout << "X" << p_x << " Y " << p_y << "\n";
             P[p_x][p_y] = 0;
         }
 
 
-
+        //Prints out map with shortest path
         std::cout << "\n\n SHORTEST PATH \n\n";
 
         for(int x = 0; x < rows; x++){
@@ -411,7 +447,8 @@ private:
         newGrid->info = info;
         map_pub.publish(*newGrid);
         */
-        
+
+        //Clearing the memory
         for (int i = 0; i < rows; ++i){
             delete [] M[i];
         }
@@ -422,7 +459,7 @@ private:
         }
         delete [] C;
 
-        
+
     }
 
 
@@ -438,7 +475,7 @@ private:
         pos_set = true;
     }
 
-    
+
 
 
 
@@ -448,7 +485,7 @@ public:
         this->n = ros::NodeHandle();
 
         // Create a publisher object, able to push messages
-        this->cmd_vel_pub = this->n.advertise<geometry_msgs::Twist>("path_following_vel", 10);
+        this->cmd_vel_pub = this->n.advertise<geometry_msgs::Twist>("reactive_vel", 10);
 
         // name_of_the_subscriber = n.subscribe("topic_name")
         this->odom_sub = n.subscribe("odom", 10, &PathFollowingController::odomCallback, this);
@@ -463,7 +500,7 @@ public:
         this->n.getParam("/p", p);
         this->n.getParam("/x_offset", x_offset);
         this->n.getParam("/y_offset", y_offset);
-        
+
     }
 
     void run(){
@@ -471,7 +508,7 @@ public:
         // Send messages in a loop
         ros::Rate loop_rate(10);
         while (ros::ok())
-        {	 
+        {
             if(path_set && pos_set){
             auto msg = calculateCommand();
 
--- src/robot_driver.cpp
+++ src/robot_driver.cpp
@@ -46,6 +46,7 @@ private:
 
     double v;
     double w;
+    float initial_x, initial_y, initial_theta;
 
     int endX, endY;
     int startX, startY;
@@ -53,10 +54,128 @@ private:
 
     //Publisher Topics
     ros::Publisher cmd_vel_pub;
+    ros::Publisher set_pose_pub;
+    ros::Publisher odom_pub;
+    ros::Publisher rgb_leds_pub;
+    ros::Publisher ir_front_sensor;
+    ros::Publisher ir_left_sensor;
+    ros::Publisher ir_right_sensor;
 
     //Subscriber Topics
+    ros::Subscriber left_dist_sub;
+    ros::Subscriber right_dist_sub;
+    ros::Subscriber front_dist_sub;
+    ros::Subscriber pose_sub;
     ros::Subscriber reactive_vel_sub;
 
+    geometry_msgs::Twist calculateCommand(){
+        auto msg = geometry_msgs::Twist();
+    }
+
+    //Callback functions for subscribers
+    void poseCallback(const geometry_msgs::Pose2D& pose_msg){
+        current_time = ros::Time::now();
+
+        //since all odometry is 6DOF we'll need a quaternion created from yaw
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pose_msg.theta);
+
+        //first, we'll publish the transform over tf - broadcasts to rviz
+        geometry_msgs::TransformStamped odom_trans;
+        odom_trans.header.stamp = current_time;
+        odom_trans.header.frame_id = "odom";
+        odom_trans.child_frame_id = "base_link";
+
+        odom_trans.transform.translation.x = pose_msg.x;
+        odom_trans.transform.translation.y = pose_msg.y;
+        odom_trans.transform.translation.z = 0.0;
+        odom_trans.transform.rotation = odom_quat;
+
+        //send the transform
+        odom_broadcaster.sendTransform(odom_trans);
+
+        //next, we'll publish the odometry message over ROS
+        nav_msgs::Odometry odom;
+        odom.header.stamp = current_time;
+        odom.header.frame_id = "odom";
+
+        //set the position
+        odom.pose.pose.position.x = pose_msg.x;
+        odom.pose.pose.position.y = pose_msg.y;
+        odom.pose.pose.position.z = 0.0;
+        odom.pose.pose.orientation = odom_quat;
+
+        //set the velocity
+        odom.child_frame_id = "base_link";
+        odom.twist.twist.linear.x = v;
+        odom.twist.twist.linear.y = 0.0;
+        odom.twist.twist.angular.z = w;
+
+        //publish the message
+        odom_pub.publish(odom);
+        last_time = current_time;
+
+    }
+
+    void leftCallback( const std_msgs::Float32::ConstPtr& msg){
+        sensor_msgs::Range ir_left;
+
+        //.data is for acces to all the data from the msg
+        float sensor_val_left = static_cast<float>(msg->data);
+        if(sensor_val_left < 0.15){
+            ROS_WARN("Collision risk! The robot is %f meters of an obsctacle, on the left side", sensor_val_left);
+        }
+
+        ir_left.header.frame_id = "base_link";
+        ir_left.radiation_type = 1;
+        ir_left.field_of_view = 0.034906585;
+        ir_left.min_range = 0.1;
+        ir_left.max_range = 0.8;
+        ir_left.range = sensor_val_left;
+
+        //Publish the left sensor value to ir_left_sensor
+        ir_left_sensor.publish(ir_left);
+
+
+
+    }
+
+    void rightCallback(const std_msgs::Float32::ConstPtr& msg){
+        sensor_msgs::Range ir_right;
+
+        float sensor_val_right = static_cast<float>(msg->data);
+        if(sensor_val_right < 0.15){
+            ROS_WARN("Collision risk! The robot is %f meters of an obsctacle, on the right side", sensor_val_right);
+        }
+
+        ir_right.header.frame_id = "base_link";
+        ir_right.radiation_type = 1;
+        ir_right.field_of_view = 0.034906585;
+        ir_right.min_range = 0.1;
+        ir_right.max_range = 0.8;
+        ir_right.range = sensor_val_right;
+
+        //Publish the right sensor value to ir_right_sensor
+        ir_right_sensor.publish(ir_right);
+    }
+
+    void frontCallback(const std_msgs::Float32::ConstPtr& msg){
+        sensor_msgs::Range ir_front;
+
+        float sensor_val_front = static_cast<float>(msg->data);
+        if(sensor_val_front < 0.15){
+            ROS_WARN("Collision risk! The robot is %f meters of an obsctacle, on the front side", sensor_val_front);
+        }
+
+        ir_front.header.frame_id = "base_link";
+        ir_front.radiation_type = 1;
+        ir_front.field_of_view = 0.034906585;
+        ir_front.min_range = 0.1;
+        ir_front.max_range = 0.8;
+        ir_front.range = sensor_val_front;
+
+        //Publish the front sensor value to ir_front_sensor
+        ir_front_sensor.publish(ir_front);
+    }
 
 
     void reactiveVelCallback(const geometry_msgs::Twist& vel_msg){
@@ -76,10 +195,18 @@ private:
     geometry_msgs::Pose2D setPose(){
         auto pose_MSG = geometry_msgs::Pose2D();
         // Set position to zero
-        pose_MSG.x = 4.0;
-        pose_MSG.y = 4.0;
-        pose_MSG.theta = 0.0;
+        pose_MSG.x = initial_x;
+        pose_MSG.y = initial_y;
+        pose_MSG.theta = initial_theta;
         return pose_MSG;
+    }
+
+    std_msgs::UInt8MultiArray setLEDs(){
+    auto rgb_MSG = std_msgs::UInt8MultiArray();
+    // Set color of led lights, the first 3 enteries are for LED_1 [255,0,0] and the last 3 for LED_2 [0,255,0]
+    // THIS IS NOT SET UP CORRECTLY, FOLLOW THIS EXAMPKE: http://alexsleat.co.uk/2011/07/02/ros-publishing-and-subscribing-to-arrays/
+    //rgb_MSG.data = [255,0,0,0,255,0];
+    return rgb_MSG;
     }
 
 
@@ -88,10 +215,20 @@ public:
     RobotDriver(){
         // Initialize ROS
         this->n = ros::NodeHandle();
+
+        this->n.getParam("/initial_x", initial_x);
+        this->n.getParam("/initial_y", initial_y);
+        this->n.getParam("/initial_theta", initial_theta);
+
         // Create a publisher object, able to push messages
         this->cmd_vel_pub = this->n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+        this->set_pose_pub = this->n.advertise<geometry_msgs::Pose2D>("set_pose", 10);
         // Create a subscriber for laser scans
-        this->reactive_vel_sub = n.subscribe("path_following_vel", 10, &RobotDriver::reactiveVelCallback, this);
+        this->reactive_vel_sub = n.subscribe("reactive_vel", 10, &RobotDriver::reactiveVelCallback, this);
+        this->pose_sub = n.subscribe("pose", 10, &RobotDriver::poseCallback, this);
+        this->left_dist_sub = n.subscribe("left_distance", 10, &RobotDriver::leftCallback, this);
+        this->right_dist_sub = n.subscribe("right_distance", 10, &RobotDriver::rightCallback, this);
+        this->front_dist_sub = n.subscribe("front_distance", 10, &RobotDriver::frontCallback, this);
 
     }
 
@@ -108,9 +245,13 @@ public:
 
             // Calculate the command to apply
             auto vel_MSG = cmdVelUpdate();
+            auto msg = calculateCommand();
+            auto rgb_MSG = setLEDs();
+            auto pose_MSG = setPose();
 
             // Publish the new command
             this->cmd_vel_pub.publish(vel_MSG);
+            this->set_pose_pub.publish(pose_MSG);
 
 
             ros::spinOnce();
